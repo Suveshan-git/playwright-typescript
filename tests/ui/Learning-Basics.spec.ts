d9a22455733bbf6e087274f89a5c128bb71e8837
Suveshan Moodley <suveshan.moodley@gmail.com>
2026-05-15 04:47:56 +0200

    Initial framework implemented with practice website and SDET learning notes.

---
+++ tests/ui/Learning-Basics.spec.ts
@@ -0,0 +1,19 @@
+/*
+    1. Syntax for writing a test:
+
+        test("title of test", async ({fixture}) => {
+            //step 1
+            //step 2
+            //step 3
+
+            //assertion
+        })")
+*/
+
+import {test, expect} from "@playwright/test";
+
+test("Verify practice website title", async ({page}) => {
+    await page.goto("https://practice.expandtesting.com/");
+
+    await expect(page).toHaveTitle("Automation Testing Practice Website for QA and Developers | UI and API");
+})
